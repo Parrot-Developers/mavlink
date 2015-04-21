4d765632d894ce5a0029c279364813ca96731c03
Jean-Baptiste Dubois <jean-baptiste.dubois@parrot.com>
2015-04-21 14:28:28 +0200

    [BUILD] add upstream-setup.sh script

---
+++ upstream-setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+readonly UPSTREAM_REPO="https://github.com/mavlink/mavlink.git"
+
+echo "Add upstream remote: ${UPSTREAM_REPO}"
+git remote add upstream ${UPSTREAM_REPO}
+git fetch upstream --no-tags
+
+echo "Setup upstream branches"
+git branch upstream-master upstream/master --track
+
